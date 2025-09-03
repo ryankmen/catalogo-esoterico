b9376b2b8fb3f62a0b5e0b523dcac3246b4ec224
ryankmen <mendozaryan1708@gmail.com>
2025-09-02 22:13:08 -0500

    Add files via upload

---
+++ netlify/functions/book.js
@@ -0,0 +1,4 @@
+// book.js
+exports.handler = async (event) => {
+  return { statusCode:200, body: JSON.stringify({ message: 'Integrar reservas aquí'}) };
+}
---
+++ netlify/functions/createCheckout.js
@@ -0,0 +1,4 @@
+// createCheckout.js
+exports.handler = async (event) => {
+  return { statusCode:200, body: JSON.stringify({ message: 'Integrar Stripe aquí'}) };
+}
